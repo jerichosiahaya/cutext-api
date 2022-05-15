b40f40a3f8691d0fdc33db74e34fa498522ae983
Jericho Siahaya <jericho.siahaya@gmail.com>
2022-05-15 11:30:15 +0900

    add cors setting

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const idr = require("./idr/index");
+const cors = require('cors');
 
+app.use(cors);
 app.use(express.json());
 app.use(
   express.urlencoded({
